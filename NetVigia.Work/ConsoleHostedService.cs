cd24bc7c0c64036d3c2c91058b69396d35d01c1f
Dravvos <daniddias53@gmail.com>
2025-07-10 15:45:51 -0300

    Troca dos Workers de Web API para Console App

---
+++ NetVigia.Work/ConsoleHostedService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NetVigia.Work.Workers.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetVigia.Work
+{
+    public class ConsoleHostedService : BackgroundService
+    {
+        private readonly ILogger<ConsoleHostedService> _logger;
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
+        private readonly ICheckScheduler _checkScheduler;
+
+        public ConsoleHostedService(ILogger<ConsoleHostedService> logger, IHostApplicationLifetime hostApplicationLifetime, ICheckScheduler checkScheduler)
+        {
+            _logger = logger;
+            _hostApplicationLifetime = hostApplicationLifetime;
+            _checkScheduler = checkScheduler;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Aplicação iniciada. Pressione Ctrl+C para sair.");
+
+            var tcs = new TaskCompletionSource<bool>();
+
+            using var reg = stoppingToken.Register(() =>
+            {
+                _logger.LogInformation("Iniciando desligamento gracioso...");
+                tcs.TrySetResult(true);
+            });
+
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                e.Cancel = true;
+                tcs.TrySetResult(true);
+            };
+
+            await tcs.Task;
+
+            try
+            {
+                // Liberar recursos antes de desligar
+                if (_checkScheduler is IDisposable disposable)
+                {
+                    disposable.Dispose();
+                }
+
+                _logger.LogInformation("Monitoramento desligado com sucesso.");
+            }
+            finally
+            {
+                _hostApplicationLifetime.StopApplication();
+            }
+        }
+    }
+}
---
+++ NetVigia.Work/Workers/Interfaces/ICheckScheduler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetVigia.DTO;
+
+namespace NetVigia.Work.Workers.Interfaces
+{
+    public interface ICheckScheduler
+    {
+        IReadOnlyCollection<Guid> GetScheduledWebsiteIds();
+        void ScheduleWebsiteCheck(ServerDTO website);
+        void UnscheduleWebsiteCheck(Guid websiteId);
+        void UpdateWebsiteCheckSchedule(ServerDTO website);
+        bool IsScheduled(Guid websiteId);
+    }
+}
