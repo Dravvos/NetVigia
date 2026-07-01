e5240b2a84a0cd98b103164bd49efe8987d1bdd9
Dravvos <daniddias53@gmail.com>
2026-07-01 12:11:40 -0300

    Remove SSL certificate handling and `GetCertificateInfo`
    Removed the `GetCertificateInfo` endpoint and its associated
    methods from `CheckController.cs` in the `NetVigia.API.Controllers`
    namespace, including logic for retrieving and validating SSL
    certificates.
    
    Removed SSL certificate handling logic from the
    `CreateSerilogMongoDBLogger` method in `Program.cs` in the
    `NetVigia.Workers` namespace, including CA and client certificate
    loading and MongoDB `SslSettings` configuration.
    
    These changes simplify the codebase by removing unused or
    unnecessary SSL certificate-related functionality.

--- NetVigia.Workers/Program.cs
+++ NetVigia.Workers/Program.cs
@@ -101,18 +101,6 @@ namespace NetVigia.Workers
             var mongoConnectionString = Environment.GetEnvironmentVariable("MongoDBConnection");
             var settings = MongoClientSettings.FromUrl(new MongoUrl(mongoConnectionString));
 
-            var caCert = new X509Certificate2(@"C:\Users\supero\mongodb-ca.crt");
-            var clientCert = new X509Certificate2(@"C:\Users\supero\mongodb-client.pfx", "YqY,&soTB_fQ!r5#",
-                 X509KeyStorageFlags.MachineKeySet |
-            X509KeyStorageFlags.PersistKeySet |
-            X509KeyStorageFlags.Exportable);
-
-            settings.SslSettings = new SslSettings
-            {
-                ClientCertificates = new List<X509Certificate> { clientCert },
-                ServerCertificateValidationCallback = (sender, cert, chain, errors) => true
-            };
-
             var client = new MongoClient(settings);
 
             var database = client.GetDatabase("NetVigiaLogs");
